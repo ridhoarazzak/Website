6079c93dd3d08c89ae1329f71560003057c415d1
ridhoarazzak <54082062+ridhoarazzak@users.noreply.github.com>
2025-06-27 22:30:57 +0700

    Create new first Footer.js

---
+++ Components/Footer.js
@@ -0,0 +1,7 @@
+export default function Footer() {
+  return (
+    <footer className="bg-[#6F4E37] text-white text-center p-4">
+      © {new Date().getFullYear()} Motel & Tourism Solok Selatan. All rights reserved.
+    </footer>
+  );
+}
